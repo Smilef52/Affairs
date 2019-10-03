b69c54c8664db5a654899d80a2fca28007800869
kaolalala <1921346727@qq.com>
2019-10-03 23:14:54 +0800

    修改了目录结构

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <bottom-column></bottom-column>
+    </div>
+</template>
+
+<script>
+import BottomColumn from "@/common/bottomColumn/BottomColumn"
+export default {
+    name: "Home",
+    components: {
+        BottomColumn
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
